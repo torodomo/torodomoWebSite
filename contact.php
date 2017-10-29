83fd5c215bf463ab879e35cbbc05b0086d60110f
Toro Roan <torodomo@me.com>
2017-10-28 21:41:11 -0700

    some changes

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'torodomo@me.com';
+$siteTitle = 'Toro Roan';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
